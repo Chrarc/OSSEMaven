06bde0eecfb8513d1196e1dda45c0fbd0b107237
Christian Oberfoell <christian.oberfoell@hs-furtwangen.de>
2021-05-01 10:51:56 +0200

    Project in Eclipse importiert, Javadoc hinzugefügt und Maven Site ausgeführt um die Dokumentation zu erstellen.

--- TextUpperCase/src/main/java/de/hfu/TextUpperCase.java
+++ TextUpperCase/src/main/java/de/hfu/TextUpperCase.java
@@ -1,7 +1,20 @@
 package de.hfu;
 import java.util.Scanner;
 
+/**
+ * Ein Programm zur Konvertierung von Textzeilen in Großbuchstaben.
+ * Bei Ausfuehrung wird eine Aufforderung zur Texteingabe angezeigt.
+ * Der konvertierte Text wird in der Kommandozeile zurueckgegeben.
+ *
+ * @author Christian Oberfoell
+ * @version 1.0
+ */
 public class TextUpperCase{
+	/**
+     * Hauptprogramm.
+     * 
+     * @param args Kommandozeilenparameter
+     */
 	public static void main(String[] args){
 		System.out.println("Bitte einen Text eingeben");
 		Scanner scanner = new Scanner(System.in);
